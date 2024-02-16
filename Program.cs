fd3f655f3b8547adeaba9479b0789bdfc00d7b5a
Rashmi Priya <priye98priya@gmail.com>
2024-02-16 10:10:43 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalcExample;
+
+//public delegate void CalcDel(double num1, double num2);
+//Calc calc = new Calc();
+//CalcDel del = CalcDel(calc.Add);
+
+//Console.WriteLine("Welcome to the number System world! ... ");
+
+
+
+Calc calc = new Calc();
+Console.WriteLine("Add Call");
+calc.Add(12, 45);
